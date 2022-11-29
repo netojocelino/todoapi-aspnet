4881d6683431577a589badd75bc21301bfeaffc5
Neto Jocelino <netoj96@live.com>
2022-11-29 13:22:15 -0300

    Extract GET functions

--- Program.cs
+++ Program.cs
@@ -5,13 +5,17 @@ using Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoDB>( opt => opt.UseInMemoryDatabase("TodoList"));
+/* TODO: why doen't work? */
 // builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
-app.MapGet("/todoitems", async (TodoDB db) => await db.Todos.ToListAsync() );
-app.MapGet("/todoitems/complete", async (TodoDB db) => await db.Todos.Where( item => item.IsComplete).ToListAsync() );
-app.MapGet("/todoitems/{id}", async (Int64 id, TodoDB db) => await db.Todos.FindAsync(id) is Todo todo ? Results.Ok(todo) : Results.NotFound() );
+/* TODO: why doen't work? */
+// var todoItems = app.MapGroup("/todoitems");
+
+app.MapGet("/todoitems", GetAllTodos );
+app.MapGet("/todoitems/complete", GetCompleteTodos );
+app.MapGet("/todoitems/{id}", GetTodoById);
 
 app.MapPost("/todoitems", async (Todo todo, TodoDB db) => {
     db.Todos.Add(todo);
@@ -48,3 +52,25 @@ app.MapDelete("/todoitems/{id}", async (Int64 id, TodoDB db) => {
 });
 
 app.Run();
+
+
+static async Task<IResult> GetAllTodos (TodoDB db) {
+    var todos  = await db.Todos.ToListAsync();
+    return Results.Ok(todos);
+}
+
+static async Task<IResult> GetCompleteTodos (TodoDB db) {
+    var todoCompleted = await db.Todos
+        .Where( item => item.IsComplete )
+        .ToListAsync();
+
+    return Results.Ok(todoCompleted);
+}
+
+static async Task<IResult> GetTodoById(Int64 id, TodoDB db) {
+    Todo? todo  = await db.Todos.FindAsync(id);
+    if (todo is null) {
+        return Results.NotFound();
+    }
+    return Results.Ok(todo);
+}
